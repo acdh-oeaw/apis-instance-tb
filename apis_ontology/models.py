a26c903e58da77f9edae44f1a92a2871228fb75b
KK <dev-kk@oeaw.ac.at>
2024-10-22 14:11:54 +0200

    feat(models): add Work, Expression models

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -1,4 +1,9 @@
 from apis_core.apis_entities.models import AbstractEntity
+from apis_core.history.models import VersionMixin
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 class BaseEntity(AbstractEntity):
     """
@@ -7,3 +12,48 @@ class BaseEntity(AbstractEntity):
 
     class Meta:
         abstract = True
+
+
+class TitlesMixin(models.Model):
+    """
+    Mixin for fields shared between work-like entities.
+    """
+
+    title = models.CharField(
+        max_length=255,
+        blank=True,
+        default="",
+        verbose_name=_("Titel"),
+    )
+
+    subtitle = models.CharField(
+        max_length=255,
+        blank=True,
+        default="",
+        verbose_name=_("Untertitel"),
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Work(VersionMixin, TitlesMixin, BaseEntity):
+    """
+    The abstract notion of an intellectual creation, irrespective
+    of its exact transmitted version, language or other form.
+    """
+
+    class Meta:
+        verbose_name = _("werk")
+        verbose_name_plural = _("werke")
+
+
+class Expression(VersionMixin, TitlesMixin, BaseEntity):
+    """
+    A concrete representation of a given Work,
+    captured in signs, images, audio signals,...
+    """
+
+    class Meta:
+        verbose_name = _("werksexpression")
+        verbose_name_plural = _("werksexpressionen")
